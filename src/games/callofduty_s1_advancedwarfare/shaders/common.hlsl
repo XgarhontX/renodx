ff70dc7e0436126454aea12c99f3edc0e59f1c9a
David Huynh <xgarhontx@gmail.com>
2025-08-17 18:29:14 -0700

    callofduty_s1_advancedwarfare & callofduty_h1_modernwarfareremastered: renamed shaders for parity

---
+++ src/games/callofduty_s1_advancedwarfare/shaders/common.hlsl
@@ -0,0 +1,235 @@
+#include "../shared.h"
+
+// (Copied from: ff7rebirth)
+// AdvancedAutoHDR pass to generate some HDR brightess out of an SDR signal.
+// This is hue conserving and only really affects highlights.
+// "sdr_color" is meant to be in "SDR range", as in, a value of 1 matching SDR white (something between 80, 100, 203, 300 nits, or whatever else)
+// https://github.com/Filoppi/PumboAutoHDR
+float3 PumboAutoHDR(float3 sdr_color) {
+  const float shoulder_pow = CUSTOM_MOV_SHOULDERPOW;
+  const float SDRRatio = max(renodx::color::y::from::BT2020(sdr_color), 0.f);
+
+  // Limit AutoHDR brightness, it won't look good beyond a certain level.
+  // The paper white multiplier is applied later so we account for that.
+  float target_max_luminance = min(RENODX_PEAK_WHITE_NITS, pow(10.f, ((log10(RENODX_DIFFUSE_WHITE_NITS) - 0.03460730900256) / 0.757737096673107)));
+  target_max_luminance = lerp(1.f, target_max_luminance, .5f);
+
+  const float auto_HDR_max_white = max(target_max_luminance / RENODX_DIFFUSE_WHITE_NITS, 1.f);
+  const float auto_HDR_shoulder_ratio = 1.f - max(1.f - SDRRatio, 0.f);
+  const float auto_HDR_extra_ratio = pow(max(auto_HDR_shoulder_ratio, 0.f), shoulder_pow) * (auto_HDR_max_white - 1.f);
+  const float auto_HDR_total_ratio = SDRRatio + auto_HDR_extra_ratio;
+  return sdr_color * renodx::math::SafeDivision(auto_HDR_total_ratio, SDRRatio, 1);  // Fallback on a value of 1 in case of division by 0
+}
+
+//https://github.com/ronja-tutorials/ShaderTutorials/blob/master/Assets/047_InverseInterpolationAndRemap/Interpolation.cginc
+float invLerp(float from, float to, float value) {
+  return (value - from) / (to - from);
+}
+float4 invLerp(float4 from, float4 to, float4 value) {
+  return (value - from) / (to - from);
+}
+float remap(float origFrom, float origTo, float targetFrom, float targetTo, float value){
+  float rel = invLerp(origFrom, origTo, value);
+  return lerp(targetFrom, targetTo, rel);
+}
+float4 remap(float4 origFrom, float4 origTo, float4 targetFrom, float4 targetTo, float4 value){
+  float4 rel = invLerp(origFrom, origTo, value);
+  return lerp(targetFrom, targetTo, rel);
+}
+
+// //https://stackoverflow.com/questions/32915724/pack-two-floats-within-range-into-one-float
+// float FloatPack_In(float a, float b) {
+//   uint aScaled = a * 0xFFFF;
+//   uint bScaled = b * 0xFFFF;
+//   uint abPacked = (aScaled << 16) | (bScaled & 0xFFFF);
+//   return asfloat(abPacked);
+// }
+// void FloatPack_Out(float inputFloat, out float a, out float b) {
+//   uint uintInput = asuint(inputFloat);
+//   a = (uintInput >> 16) / 65535.0f;
+//   b = (uintInput & 0xFFFF) / 65535.0f;
+// }
+
+// uint R5G6B5_Get255Int(float color){
+//   return clamp(color * 255, 0, 255);
+// }
+
+// float R5G6B5_Get01Float(uint color) {
+//   return (color / 255.f);
+// }
+// // 0000 0000 0000 0000
+// // 0b 0000 0000 1111 1111
+
+// //red
+// // 0b 0000 0000 1111 1000 //mask 5
+// // 0b 1111 1000 0000 0000 //mask 5 << 8
+
+// //green
+// // 0b ---- -000 1111 1100 //mask 6
+// // 0b ---- -111 1110 0000 //mask 6 << 3
+
+// //blue
+// // 0b ---- ---- 1111 1000 //mask 5
+// // 0b ---- ---- ---1 1111 //mask 5 >> 3
+
+
+// //https://github.com/GPUOpen-LibrariesAndSDKs/nBodyD3D12/blob/master/MiniEngine/Core/Shaders/PixelPacking.hlsli
+// //https://barth-dev.de/about-rgb565-and-how-to-convert-into-it/
+// uint R5G6B5_Pack(float3 color)
+// {
+//   color = renodx::color::srgb::Encode(color);
+
+//   uint r,g,b;
+//   r = R5G6B5_Get255Int(color.r);
+//   g = R5G6B5_Get255Int(color.g);
+//   b = R5G6B5_Get255Int(color.b);
+
+//   // r = (r & 0b0000000011111000) << 8;
+//   // g = (g & 0b0000000011111100) << 3;
+//   // b = (b & 0b0000000011111000) >> 3;
+//   r = (r & 248) << 8;
+//   g = (g & 252) << 3;
+//   b = (b & 248) >> 3;
+  
+//   return r | g | b;
+//   // return asfloat(r | g | b);
+// }
+
+// float3 R5G6B5_Unpack(float4 r0)
+// {
+//   uint r,g,b;
+//   uint rgb = r0.w;
+
+//   // r = (rgb & 0b1111100000000000) >> 8;
+//   // g = (rgb & 0b0000011111100000) >> 3;
+//   // b = (rgb & 0b0000000000011111) << 3;
+//   r = (rgb & 63488) >> 8;
+//   g = (rgb & 2016) >> 3;
+//   b = (rgb & 31) << 3;
+
+//   return renodx::color::srgb::Decode(float3(R5G6B5_Get01Float(r), R5G6B5_Get01Float(g), R5G6B5_Get01Float(b)));
+// }
+
+float Tonemap_GetY(float3 color) {
+  return renodx::color::y::from::BT709(color);
+}
+
+void Tonemap_SaveBlacks( in float3 colorUntonemapped, inout float4 r0) {
+  // if (CUSTOM_UPGRADETONEMAP_SAVEBLACKS == 0) return;
+  if (Tonemap_GetY(r0.xyz) <= CUSTOM_UPGRADETONEMAP_SAVEBLACKS)
+    r0.xyz = renodx::color::correct::Luminance(colorUntonemapped, Tonemap_GetY(colorUntonemapped), CUSTOM_UPGRADETONEMAP_SAVEBLACKS);
+}
+
+
+void Tonemap_UpgradeTonemap0(inout float3 colorUntonemapped, in float4 r0) {
+  if (RENODX_TONE_MAP_TYPE == 0) return;
+
+  //save blacks
+  Tonemap_SaveBlacks(colorUntonemapped, r0);
+
+  //upgrade
+  colorUntonemapped = renodx::tonemap::UpgradeToneMap(
+    colorUntonemapped, r0.xyz, r0.xyz, 
+    CUSTOM_UPGRADETONEMAP_POSTPROCESS, CUSTOM_UPGRADETONEMAP_AUTO
+  );
+}
+
+
+void ADSSights_Scale(inout float4 o0) {
+  o0.xyz *= CUSTOM_ADSSIGHT_MULTIPLIER;
+}
+
+void Tonemap_BloomScale(inout float4 color) {
+  color.xyz *= CUSTOM_BLOOM_MULTIPLIER; 
+  color.w = 1;
+}
+
+float PreExposure_GetOffset(float4 v) {
+  float result;
+  // switch (CUSTOM_PREEXPOSURE_OFFSET_MODE) {
+  //   case 0: result = v.x; break;
+  //   case 1: result = v.y; break;
+  //   default: result = v.z; break;
+  // }
+  result = max(v.y, v.z);
+  result *= CUSTOM_PREEXPOSURE_OFFSET_MULTIPLIER;
+  return result;
+}
+
+float PreExposure_GetAutoexposure(float4 v) {
+  float result;
+  // switch (CUSTOM_PREEXPOSURE_AUTO_MODE) {
+  //   case 0: result = v.x; break;
+  //   case 1: result = v.y; break;
+  //   default: result = v.z; break;
+  // }
+  result = v.y;
+  result *= CUSTOM_PREEXPOSURE_AUTO_MULTIPLIER;
+  return result;
+}
+
+float Tonemap_CalculatePreExposureMultiplier(float4 thresholds, float4 autoexp/*, float4 v2*/) 
+{
+  //v0.xyz = high, mid, low thresholds
+  //v1.xyz = high, mid, low multipliers
+  //v2 = color corrections?
+
+  float target;
+  target = PreExposure_GetOffset(thresholds) + PreExposure_GetAutoexposure(autoexp);
+  target *= CUSTOM_PREEXPOSURE_FINAL;
+  return target;
+}
+
+void Tonemap_PrepColorUntonemapped(inout float3 colorUntonemapped, inout float4 r0) {
+  colorUntonemapped = r0.xyz;
+  colorUntonemapped = renodx::color::correct::Luminance(colorUntonemapped, Tonemap_GetY(colorUntonemapped), r0.w);
+  // colorUntonemapped = max(float3(0,0,0), colorUntonemapped); //clamp
+  r0.w = 1; //reset
+}
+
+void Tonemap_RecoverSDR(inout float4 r0) {
+  r0.xyz = renodx::color::correct::Luminance(r0.xyz, Tonemap_GetY(r0.xyz), r0.w, 1); //correct
+  r0.w = 1; //reset
+}
+
+// float3 Tonemap_Compressor(float3 x) {
+//   x /= CUSTOM_COMPRESSOR_GAIN;
+
+//   float colorY = renodx::color::y::from::BT709(x);
+//   x = colorY < 1 ? x : renodx::math::SignPow(x, CUSTOM_COMPRESSOR_POW); 
+
+//   x *= CUSTOM_COMPRESSOR_GAIN;
+//   return x;
+// }
+// float Tonemap_Compressor(float x) {
+//   x /= CUSTOM_COMPRESSOR_GAIN;
+//   x = x < 1 ? x : renodx::math::SignPow(x, CUSTOM_COMPRESSOR_POW); 
+//   x *= CUSTOM_COMPRESSOR_GAIN;
+//   return x;
+// }
+
+static renodx::debug::graph::Config graph_config; //performance? mt safe?
+void Tonemap_Do(inout float4 r0, float3 colorUntonemapped, float3 colorTonemapped, float3 colorSDRNeutral, float2 uv, Texture2D<float4> texColor) {
+  if (RENODX_TONE_MAP_TYPE > 0) {
+    //graph start
+    if (CUSTOM_IS_CALIBRATION) graph_config = renodx::debug::graph::DrawStart(uv, colorUntonemapped, texColor, RENODX_PEAK_WHITE_NITS, RENODX_DIFFUSE_WHITE_NITS);
+
+    //decode
+    // colorSDRNeutral = renodx::color::srgb::DecodeSafe(colorSDRNeutral);
+    colorTonemapped = renodx::color::srgb::DecodeSafe(colorTonemapped);
+
+    //ToneMapPass
+    r0.xyz = renodx::draw::ToneMapPass(colorUntonemapped, colorTonemapped, colorSDRNeutral);
+
+    //graph end
+    if (CUSTOM_IS_CALIBRATION) r0.xyz = renodx::debug::graph::DrawEnd(r0.xyz, graph_config);
+  } else {
+    //just decode original
+    r0.xyz = renodx::color::srgb::DecodeSafe(r0.xyz);
+    r0.xyz = saturate(r0.xyz); //emulate SDR 0-1. yes, i checked, they do rely on the inherit r8g8b8 clamping.
+  }
+
+  //RenderIntermediatePass
+  r0.xyz = renodx::draw::RenderIntermediatePass(r0.xyz);
+  r0 = max(r0, float4(0,0,0,0));
+}
